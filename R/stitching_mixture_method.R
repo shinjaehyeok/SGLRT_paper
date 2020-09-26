a047e4c1a31b8bc5880580b2f5eea9c3ae1faadb
Jaehyeok Shin <shinjaehyeok@gmail.com>
2020-09-25 20:33:45 -0700

    Refactoring SGLR_CI function

--- R/stitching_mixture_method.R
+++ R/stitching_mixture_method.R
@@ -143,8 +143,12 @@ SGLR_CI_additive <- function(alpha,
 #'   \item{dis_mix_fn}{R function for the discrete mixture bound.}
 #'   \item{log_GLR_like_stat_generator}{R function to generate log of GLR-like statistic minus the threshold.}
 #'   \item{log_dis_mart_generator}{R function to generate log of GLR-like statistic minus the threshold.}
+#'   \item{alpha}{alpha valud used to construct GLR-like and discrete mixture bound functions}
+#'   \item{g}{The boundary value for initial \code{nmin} and \code{nmax}.}
 #'   \item{eta}{The eta value used to construct underlying martingales}
 #'   \item{K}{The number of LR-like martingales used to construct bounds for \eqn{n \geq n_{\min}} part.}
+#'   \item{CI_grid}{The numeric vector for used to compute bounds.}
+#'   \item{mu_range}{Minimum and maximum of the mean parater space.}
 #' }
 #'
 #' @export
@@ -164,50 +168,52 @@ SGLR_CI <- function(alpha,
                     mu_lower = NULL,
                     mu_upper = NULL)
 {
-  # Calculate g, eta, K
+  # Calculate g, eta, K for common parameters for both generators
   param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
   g <- param_out$g
   eta <- param_out$eta
   K <- param_out$K
 
+  # Initializing function for statistic generators
+  initial_fn <- function(mu_0, is_pos){
+    # Define  the trivial Statistic for bounded mean parameter case
+    trivial_stat_fn <- function(x_bar, v){
+      out <- ifelse(x_bar == mu_0, -Inf, Inf)
+      return(out)
+    }
 
-  # Compute GLR-like statistics
-  GLR_like_stat_generator <- function(mu_0, is_pos = TRUE){
+    # If mu_0 is at the boundary, the generator return a trivial statistics
     if (!is.null(mu_upper)){
       if (mu_0 == mu_upper){
-        GLR_like_stat_fn <- function(x_bar, v){
-          out <- ifelse(x_bar == mu_0, -Inf, Inf)
-          return(out)
-        }
-        return(GLR_like_stat_fn)
+        return(list(is_trivial = TRUE, stat_fn = trivial_stat_fn))
       }
     }
     if (!is.null(mu_lower)){
       if (mu_0 == mu_lower){
-        GLR_like_stat_fn <- function(x_bar, v){
-          out <- ifelse(x_bar == mu_0, -Inf, Inf)
-          return(out)
-        }
-        return(GLR_like_stat_fn)
+        return(list(is_trivial = TRUE, stat_fn = trivial_stat_fn))
       }
     }
 
 
-    if (is_pos){
-      z_fn <- function(d) breg_pos_inv(d, mu_0)
+    if (is_pos){ # Compute statistic for the right-sided case (mu_1 > mu_0)
+      # Check whether nmin is large enough for the bounded mean space case.
       if (!is.null(mu_upper)){
         eff_nmin <- g / breg(mu_upper, mu_0)
+        # If nmin is too small, update nmin and nmax.
         if (nmin < eff_nmin){
           nmin <- eff_nmin
           nmax <- ifelse(nmax > nmin, nmax, nmin)
+          # Update boundary value for the updated nmin and nmax
           param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
           g <- param_out$g
           eta <- param_out$eta
           K <- param_out$K
         }
       }
-    } else {
-      z_fn <- function(d) breg_neg_inv(d, mu_0)
+      # Define mean mapping for the left_sided case (mu_1 < mu_0)
+      z_fn <- function(d) breg_pos_inv(d, mu_0)
+    } else { # Compute statistic for the left_sided case (mu_1 < mu_0)
+      # Check whether nmin is large enough for the bounded mean space case.
       if (!is.null(mu_lower)){
         eff_nmin <- g / breg(mu_lower, mu_0)
         if (nmin < eff_nmin){
@@ -219,8 +225,43 @@ SGLR_CI <- function(alpha,
           K <- param_out$K
         }
       }
+      # Define mean mapping for the left_sided case (mu_1 < mu_0)
+      z_fn <- function(d) breg_neg_inv(d, mu_0)
+    }
+    out <- list(is_trivial = FALSE,
+                params = param_out,
+                nmin = nmin,
+                nmax = nmax,
+                z_fn = z_fn)
+    return(out)
+  }
+
+
+  # Compute GLR-like statistics
+  GLR_like_stat_generator <- function(mu_0, is_pos = TRUE){
+    # Initialize parameters
+    initial_out <- initial_fn(mu_0, is_pos)
+    if (initial_out$is_trivial){
+      out <- list(stat_fn = initial_out$stat_fn,
+                  is_trivial = TRUE,
+                  alpha = alpha,
+                  nmax = nmax,
+                  nmin = nmin,
+                  g = g)
+      return(out)
     }
 
+    # Set parameters
+    g <- initial_out$params$g
+    eta <- initial_out$params$eta
+    K <- initial_out$params$K
+    nmin <- initial_out$nmin
+    nmax <- initial_out$nmax
+
+    # Get the mean maping
+    z_fn <- initial_out$z_fn
+
+    # Compute GLR stat
     d2 <- g / nmin
     d1 <- g / nmax
 
@@ -246,64 +287,52 @@ SGLR_CI <- function(alpha,
       }
       return(max(out,0) * v -g)
     }
-    return(GLR_like_stat_fn)
+    out <- list(stat_fn = GLR_like_stat_fn,
+                is_trivial = FALSE,
+                alpha = alpha,
+                nmax = nmax,
+                nmin = nmin,
+                g = g)
+    return(out)
   }
 
 
 
   # Compute discrete mixture statistics
-  if (nmin == 1){
-    g_eta_vec <-  g / eta^seq(1,K)
-    dis_mart_generator <- function(mu_0, is_pos = TRUE){
-      if (!is.null(mu_upper)){
-        if (mu_0 == mu_upper){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (!is.null(mu_lower)){
-        if (mu_0 == mu_lower){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (is_pos){
-        z_fn <- function(d) breg_pos_inv(d, mu_0)
-        if (!is.null(mu_upper)){
-          eff_nmin <- g / breg(mu_upper, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
+  dis_mart_generator <- function(mu_0, is_pos = TRUE){
+    # Initialize parameters
+    initial_out <- initial_fn(mu_0, is_pos)
+    if (initial_out$is_trivial) {
+      out <- list(stat_fn = initial_out$stat_fn,
+                  is_trivial = TRUE,
+                  alpha = alpha,
+                  nmax = nmax,
+                  nmin = nmin,
+                  g = g)
+      return(out)
+    }
 
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-            eta <- param_out$eta
-            K <- param_out$K
-            g_eta_vec <-  g / eta^seq(1,K)
-          }
-        }
-      } else {
-        z_fn <- function(d) breg_neg_inv(d, mu_0)
-        if (!is.null(mu_lower)){
-          eff_nmin <- g / breg(mu_lower, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
+    # Set parameters
+    g <- initial_out$params$g
+    eta <- initial_out$params$eta
+    K <- initial_out$params$K
+    nmin <- initial_out$nmin
+    nmax <- initial_out$nmax
 
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-            eta <- param_out$eta
-            K <- param_out$K
-            g_eta_vec <-  g / eta^seq(1,K)
-          }
-        }
+    # Get the mean maping
+    z_fn <- initial_out$z_fn
+
+    # Compute dis. mixture statistic
+    if (nmin == nmax){ # Case 1. nmin = nmax: Use single LR-like stat
+      d1 <- g / nmax
+      z_val <- z_fn(d1)
+      slop_val <- breg_derv(z_val, mu_0)
+
+      dis_mart <- function(x_bar, v){
+        v * ( d1 + slop_val * (x_bar - z_val) ) - g
       }
+    } else if (nmin == 1) { # Case 2. 1 = nmin < nmax : Use K LR-like stat
+      g_eta_vec <-  g / eta^seq(1,K)
       z_vec <- sapply(g_eta_vec, z_fn)
       slop_vec <- sapply(z_vec, function(z) breg_derv(z, mu_0))
 
@@ -313,115 +342,8 @@ SGLR_CI <- function(alpha,
         out <- inner_max + log(sum(exp(inner - inner_max))) - g / eta
         return(out)
       }
-      return(dis_mart)
-    }
-  } else if (nmin == nmax){
-    dis_mart_generator <- function(mu_0, is_pos = TRUE){
-      if (!is.null(mu_upper)){
-        if (mu_0 == mu_upper){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (!is.null(mu_lower)){
-        if (mu_0 == mu_lower){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (is_pos){
-        z_fn <- function(d) breg_pos_inv(d, mu_0)
-        if (!is.null(mu_upper)){
-          eff_nmin <- g / breg(mu_upper, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
-
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-            d1 <- g / nmin
-          }
-        }
-      } else {
-        z_fn <- function(d) breg_neg_inv(d, mu_0)
-        if (!is.null(mu_lower)){
-          eff_nmin <- g / breg(mu_lower, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
-
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-          }
-        }
-      }
-      d2 <- g / nmin
-      z_val <- z_fn(d2)
-      slop_val <- breg_derv(z_val, mu_0)
-
-      dis_mart <- function(x_bar, v){
-        v * ( d2 + slop_val * (x_bar - z_val) ) - g
-      }
-      return(dis_mart)
-    }
-  } else {
-    g_eta_vec <-  g / eta^seq(0,K) / nmin
-    dis_mart_generator <- function(mu_0, is_pos = TRUE){
-      if (!is.null(mu_upper)){
-        if (mu_0 == mu_upper){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (!is.null(mu_lower)){
-        if (mu_0 == mu_lower){
-          dis_mart <- function(x_bar, v){
-            out <- ifelse(x_bar == mu_0, -Inf, Inf)
-            return(out)
-          }
-          return(dis_mart)
-        }
-      }
-      if (is_pos){
-        z_fn <- function(d) breg_pos_inv(d, mu_0)
-        if (!is.null(mu_upper)){
-          eff_nmin <- g / breg(mu_upper, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
-
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-            eta <- param_out$eta
-            K <- param_out$K
-            g_eta_vec <-  g / eta^seq(0,K) / nmin
-          }
-        }
-      } else {
-        z_fn <- function(d) breg_neg_inv(d, mu_0)
-        if (!is.null(mu_lower)){
-          eff_nmin <- g / breg(mu_lower, mu_0)
-          if (nmin < eff_nmin){
-            nmin <- eff_nmin
-            nmax <- ifelse(nmax > nmin, nmax, nmin)
-
-            param_out <- const_boundary_cs(alpha, nmax, nmin, m_upper)
-            g <- param_out$g
-            eta <- param_out$eta
-            K <- param_out$K
-            g_eta_vec <-  g / eta^seq(0,K) / nmin
-          }
-        }
-      }
+    } else { # Case 3. 1 < nmin < nmax : add the base line LR-like to the  K LR-like,
+      g_eta_vec <-  g / eta^seq(0,K) / nmin
       z_vec <- sapply(g_eta_vec, z_fn)
       slop_vec <- sapply(z_vec, function(z) breg_derv(z, mu_0))
 
@@ -433,15 +355,25 @@ SGLR_CI <- function(alpha,
         out <- inner_max + log(sum(exp(inner - inner_max)))
         return(out)
       }
-      return(dis_mart)
     }
+    out <- list(stat_fn = dis_mart,
+                is_trivial = FALSE,
+                alpha = alpha,
+                nmax = nmax,
+                nmin = nmin,
+                g = g)
+    return(out)
   }
 
   # Compute CI bound
 
   find_CI_fn <- function(stat_fn, x_bar, v, is_pos = TRUE){
+
+    # Define the test function
     f_mu <- function(mu) stat_fn(x_bar, v, mu, is_pos)
-    if (is.null(CI_grid)){
+
+    if (is.null(CI_grid)){ # Compute the CI bound by using the binary search
+      # Compute the multiply factor for the searching range.
       thres <- log(1 / alpha) / v
       if (v < nmin){
         m_factor <- max(100, 100 * log(nmin / v, base = 10))
@@ -457,7 +389,7 @@ SGLR_CI <- function(alpha,
           z_bound_with_sign <- breg_neg_inv(thres, x_bar) - x_bar
           search_range <- c(x_bar + m_factor * z_bound_with_sign,
                             x_bar + z_bound_with_sign / 3)
-        } else {
+        } else { # If mu_lower is provided, use the naive search range
           search_range <- c(mu_lower, x_bar)
           if (f_mu(search_range[1]) <= 0) return(mu_lower)
         }
@@ -466,16 +398,16 @@ SGLR_CI <- function(alpha,
           z_bound_with_sign <- breg_pos_inv(thres, x_bar) - x_bar
           search_range <- c(x_bar + z_bound_with_sign / 3,
                             x_bar + m_factor * z_bound_with_sign)
-        } else{
+        } else{ # If mu_upper is provided, use the naive search range
           search_range <- c(x_bar, mu_upper)
           if (f_mu(search_range[2]) <= 0) return(mu_upper)
         }
       }
-
+      # Find the boundary of mean space
       bound <- stats::uniroot(f_mu,
                               search_range,
                               tol = 1e-12)$root
-    } else {
+    } else { # Compute the CI bound by using the grid-search
       grid_n <- length(CI_grid)
       if (is_pos){
         grid_inter <- CI_grid[CI_grid <= x_bar]
@@ -489,39 +421,39 @@ SGLR_CI <- function(alpha,
         bound <- grid_inter[min(max(ind) + 1, length(grid_inter))]
       }
     }
-  return(bound)
-}
-
-# Compute GLR-like CI bound
-
-GLR_like_fn <- function(x_bar, v, is_pos = TRUE){
-  GLR_like_inner <- function(x_bar, v, mu_0, is_pos){
-    f <- GLR_like_stat_generator(mu_0, is_pos)
-    return(f(x_bar, v))
+    return(bound)
   }
-  find_CI_fn(GLR_like_inner , x_bar, v, is_pos)
-}
 
-# Compute dis. mixture CI bound
-dis_mix_fn <- function(x_bar, v, is_pos = TRUE){
-  dis_mix_inner <- function(x_bar, v, mu_0, is_pos){
-    f <- dis_mart_generator(mu_0, is_pos)
-    return(f(x_bar, v))
+  # Compute GLR-like CI bound
+
+  GLR_like_fn <- function(x_bar, v, is_pos = TRUE){
+    GLR_like_inner <- function(x_bar, v, mu_0, is_pos){
+      f <- GLR_like_stat_generator(mu_0, is_pos)$stat_fn
+      return(f(x_bar, v))
+    }
+    find_CI_fn(GLR_like_inner , x_bar, v, is_pos)
   }
-  find_CI_fn(dis_mix_inner, x_bar, v, is_pos)
-}
 
-out <- list(GLR_like_fn = GLR_like_fn,
-            dis_mix_fn = dis_mix_fn,
-            log_GLR_like_stat_generator = GLR_like_stat_generator,
-            log_dis_mart_generator = dis_mart_generator,
-            alpha = alpha,
-            nmax = nmax,
-            nmin = nmin,
-            g = g,
-            eta = eta,
-            K = K,
-            CI_grid = CI_grid,
-            mu_range = c(mu_lower, mu_upper))
-return(out)
+  # Compute dis. mixture CI bound
+  dis_mix_fn <- function(x_bar, v, is_pos = TRUE){
+    dis_mix_inner <- function(x_bar, v, mu_0, is_pos){
+      f <- dis_mart_generator(mu_0, is_pos)$stat_fn
+      return(f(x_bar, v))
+    }
+    find_CI_fn(dis_mix_inner, x_bar, v, is_pos)
+  }
+
+  out <- list(GLR_like_fn = GLR_like_fn,
+              dis_mix_fn = dis_mix_fn,
+              log_GLR_like_stat_generator = GLR_like_stat_generator,
+              log_dis_mart_generator = dis_mart_generator,
+              alpha = alpha,
+              nmax = nmax,
+              nmin = nmin,
+              g = g,
+              eta = eta,
+              K = K,
+              CI_grid = CI_grid,
+              mu_range = c(mu_lower, mu_upper))
+  return(out)
 }
